1a6a97080035c9a8bf6729e6d73dd45236a27d73
Andrew Butcher <abutcher@redhat.com>
2015-08-23 20:51:58 -0400

    Switch to all.

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
         try:
             cur = conn.cursor()
             cur.execute("SELECT * from autopatch_server")
-            servers = cur.fetchmany(5)
+            servers = cur.fetchmall()
             cur.close()
         except:
             logging.debug("Failed to retrieve server list.")
